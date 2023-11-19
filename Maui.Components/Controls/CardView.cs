8f567619751c46f33babad9a0e5193c1b4d969e6
Francisco Funes <franke1615@gmail.com>
2023-11-18 18:10:27 -0800

    started on card view

---
+++ Maui.Components/Controls/CardView.cs
@@ -0,0 +1,25 @@
+﻿namespace Maui.Components.Controls;
+
+public class CardView : ContentView
+{
+    #region Events
+    public event EventHandler Clicked;
+    #endregion
+
+    #region Public Properties
+    public static readonly BindableProperty ImageSourceProperty = BindableProperty.Create(
+        nameof(ImageSourceProperty),
+        typeof(ImageSource),
+        typeof(CardView),
+        null
+    );
+
+    public ImageSource ImageSource 
+    {
+        get => (ImageSource) GetValue(ImageSourceProperty);
+        set => SetValue(ImageSourceProperty, value);
+    }
+
+    
+    #endregion
+}
