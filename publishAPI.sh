ac7c5aa72343c112aea62793b928454b9a0309ae
francisco <franke1615@gmail.com>
2024-06-29 08:50:57 -0700

    small update to bash script

--- publishAPI.sh
+++ publishAPI.sh
@@ -3,6 +3,5 @@ dotnet restore Inventory.API
 dotnet publish Inventory.API -c release -r linux-x64 --self-contained
 
 # the following will only work when rsa passwordless auth is setup to target machine
-# inventory_user must also have rights to /var/www/inventoryApp folder
-DIR="$( cd "$( dirname "$0" )" && pwd )"
-scp -r $DIR/Inventory.API/bin/Release/net8.0/linux-x64/publish inventory_user@192.168.1.141:/var/www/inventoryApp
+# inventory_user must also have rights to \\var\\www\\inventoryApp folder
+scp -r C:\\Projects\\MAUI.Inventory\\Inventory.API\\bin\\Release\\net8.0\\linux-x64\\publish dev@192.168.1.28:\\var\\www\\inventoryApp
